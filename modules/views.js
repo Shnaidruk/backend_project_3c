d61506a655a819a7c4b3ab65d9b4d2e28f5bb033
Shnaidruk <ashnaidruk56@gmail.com>
2023-12-20 18:24:42 +0200

    Add .gitignore, endpoints:POST, GET, DELETE for record

--- modules/views.js
+++ modules/views.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const users = [];
 const categories = [];
-
+const records = [];
 
 router.post('/user', (req,res) =>{
     const {user_name} = req.body;
@@ -84,7 +84,7 @@ router.get('/category/<cat_id>', (req, res) =>{
     res.status(200).json(curCat);
 });
 
-router.delete('/category', (req, res) => {
+router.delete('/category/<cat_id>', (req, res) => {
     const cId = req.params.id;
 
     const curCat = categories.find(category => category.id === cId);
@@ -99,6 +99,53 @@ router.delete('/category', (req, res) => {
 });
 
 
+router.post('/record', (req, res) => {
+    const {uId, cId, amount} = req.body;
+
+    const user = users.find(user => user.id === uId);
+    const category = categories.find(category => category.id === cId);
+
+    if (!user || !category){
+        return res.status(400).json({error: 'Invalid input'});
+    }
+
+    const rec_id = uuidv4();
+    const record = {rec_id, uId, cId, amount, timestamp: Date.now().toString()};
+
+    records.push(record);
+
+    res.status(201).json(record);
+});
+
+router.get('/record', (req,res) => {
+    const{user_id, cat_id} = req.query;
+
+    if (!user_id && !category_id) {
+        return res.status(400).json({ error: 'Please provide user_id and/or category_id' });
+    }
+
+    const filteredExpenses = records.filter(record =>
+        (!user_id || record.uId === user_id) &&
+        (!cat_id || record.cId === cat_id)
+    );
+
+    res.status(200).json(filteredExpenses);
+
+} );
+
+router.delete('/record/<rec_id>', (req,res) => {
+    const rId = req.params.id;
+
+    const curRec = records.find(record => record.id === rId);
+
+    if (!curRec){
+        return res.status(404).json({error: 'No category with such rec_id'})
+    }
+
+    const delR = records.splice(curRec, 1)[0];
+
+    res.status(200).json(delR);
+});
 
 
 router.get('/healthcheck', (req, res) => {
